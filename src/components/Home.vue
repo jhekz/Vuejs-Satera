53e60d29853e15a8fe850454e7d18063ef6a4142
jhekz <ramadhanfhujjar@gmail.com>
2019-01-21 17:50:46 +0800

    revisi

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,15 @@
 <template>
 <div class="mar">
-<div class="container" style="text-align: center;">
+<div class="mar">
+<b-row>
+<b-col cols="3">
+<b-card bg-variant="dark" text-variant="white">
+<b-container>
+<b-card-body style="height:545px;">
+<p class="card-text">
+<h2 align="center">Menu</h2>
+<hr/>
 <ul class="nav flex-column">
-<li class="nav-item">
-<a class="nav-link menus"><router-link to="/daftar">Daftar</router-link></a>
-</li>
 <li class="nav-item">
 <a class="nav-link menus"><router-link to="/login">Login</router-link></a>
 </li>
@@ -15,6 +20,38 @@
 <a class="nav-link menus"><router-link to="/tentang">Tentang</router-link></a>
 </li>
 </ul>
+</b-card-body>
+</b-container>
+</b-card>
+</b-col>
+<b-col style="background-color:white;">
+<b-container>
+<v-jumbotron>
+<v-container fill-height>
+<v-layout align-center>
+<v-flex>
+<h3 class="display-3">Belajar Satera Jontal</h3>
+<span class="subheading">
+Aplikasi pembelajaran Satera Jontal Berbasis Web yang memungkinkan anda mengenal dan mempelajari Satera Jontal melalui pendekatan gamifikasi.
+</span>
+<v-divider class="my-3"></v-divider>
+<div class="title mb-3">Gabung Sekarang, gratis!</div>
+<v-btn
+class="mx-0"
+color="teal"
+large
+dark
+to="/daftar"
+>
+Register
+</v-btn>
+</v-flex>
+</v-layout>
+</v-container>
+</v-jumbotron>
+</b-container>
+</b-col>
+</b-row>
 </div>
 </div>
 </template>
@@ -33,13 +70,12 @@ export default {
 <style scoped>
 .menus{
 font-size: 20pt;
-color:cyan;
+}
+a{
+color:white;
 }
 a:hover{
 text-decoration: none;
-list-style-position: outside;
-list-style-image: url(./../../static/img/icon/arrow.png);
-font-size: 30pt;
 font-weight: bold;
 transition: 0.5s;
 color:red;
@@ -53,7 +89,6 @@ left: 0%;
 }
 }
 .mar{
-margin-top: 150px;
 animation: 1s move;
 position: relative;
 }
